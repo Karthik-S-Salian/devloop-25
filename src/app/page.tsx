6f4e7c0764512e3c54e23ed841e185f69259e9a1
len-mendonca <lenmendonca2003@gmail.com>
2024-09-11 08:13:14 +0530

    update 2: puzzle 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,49 @@
 import Link from "next/link";
+import path from "path";
 
 export default function Home() {
-	const puzzleListRound1 = [
-		{ path:"/puzzle1", title:"responsive puzzle"},
-		{ path:"/puzzle2", title:"Quizzes"},
-		{ path:"/puzzle3", title:"A simple click"},
-		{ path:"/puzzle4", title:"Solve Qr code"},
-		{ path:"/puzzle5", title:"Solve an Excecl file"},
-		{ path:"/puzzle6", title:"Who am I?"},
-		// { path:"/puzzle7", title:""},
-	] 
-	const puzzleListRound2 = [
-		{ path:"/jwt", title:"JWT puzzle"},
-		// { path:"/puzzle", title:""},
-	]
+  const puzzleListRound1 = [
+    { path: "/puzzle1", title: "responsive puzzle" },
+    { path: "/puzzle2", title: "Quizzes" },
+    { path: "/puzzle3", title: "A simple click" },
+    { path: "/puzzle4", title: "Solve Qr code" },
+    { path: "/puzzle5", title: "Solve an Excecl file" },
+    { path: "/puzzle6", title: "Who am I?" },
+    { path: "/puzzle7", title: "IPSum" },
+    { path: "/puzzle8", title: "reverse audio" },
+    // { path:"/puzzle9", title:""},
+  ];
+  const puzzleListRound2 = [
+    { path: "/jwt", title: "JWT puzzle" },
+    // { path:"/puzzle", title:""},
+  ];
 
   return (
-  	<main className="p-24">
-    	<h2 className="text-center mb-12">Puzzle List</h2>
-		<p>Round1 puzzles</p>
-     	<ol>
-				{puzzleListRound1.map((puzzle,i)=> 
-					<li>
-						<Link href={puzzle.path} className="underline text-blue-500 italic">{i+1}. {puzzle.title}</Link>
-					</li>)} 
-     	</ol>
+    <main className="p-24">
+      <h2 className="mb-12 text-center">Puzzle List</h2>
+      <p>Round1 puzzles</p>
+      <ol>
+        {puzzleListRound1.map((puzzle, i) => (
+          <li>
+            <Link href={puzzle.path} className="italic text-blue-500 underline">
+              {i + 1}. {puzzle.title}
+            </Link>
+          </li>
+        ))}
+      </ol>
 
-     	<br />
+      <br />
 
-     	<p>Round2 puzzles</p>
-     	<ul>
-			 {puzzleListRound2.map((puzzle,i)=> 
-					<li>
-						<Link href={puzzle.path} className="underline text-blue-500 italic">{i+1}. {puzzle.title}</Link>
-					</li>)} 
-     	</ul>
+      <p>Round2 puzzles</p>
+      <ul>
+        {puzzleListRound2.map((puzzle, i) => (
+          <li>
+            <Link href={puzzle.path} className="italic text-blue-500 underline">
+              {i + 1}. {puzzle.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
