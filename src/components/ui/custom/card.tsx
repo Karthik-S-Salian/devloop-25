73e2fbbebb0a366b0e066050e4dee0da85e8f02e
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-10-19 01:28:31 +0530

    [fix] team image ui

--- src/components/ui/custom/card.tsx
+++ src/components/ui/custom/card.tsx
@@ -1,15 +1,14 @@
-interface TeamCardProps {
-  name: string;
-  image: string;
-}
-const TeamCard = ({ name, image }: TeamCardProps) => {
+import Image from "next/image";
+
+const TeamCard = ({ name, image }: { name: string; image: string }) => {
   return (
     <div className="border-1 flex h-[400px] w-[270px] flex-col items-center gap-4 rounded-2xl border border-yellow-600 px-3 py-4 shadow-lg shadow-yellow-300">
-      <div className="w-full">
-        <img
+      <div className="relative h-[250px] w-full">
+        <Image
           src={image}
           alt={name}
-          className="h-[250px] max-h-[250px] w-full rounded-xl"
+          className="rounded-xl object-cover"
+          fill
         />
       </div>
       <h1 className="font-fira-code text-2xl capitalize">{name}</h1>
