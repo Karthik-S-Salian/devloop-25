849df936a33fd1cf60473e0f74faf48fc7b5e803
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-10-18 18:48:51 +0530

    C:/Program Files/Git/story button url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,9 +117,11 @@ const Home = () => {
               SignIn here
             </button>
           </Link>
+          <Link href="/story">
           <button className="text-md mb-2 me-2 bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center font-normal text-white shadow-lg shadow-green-800/80 ring-4 ring-green-900 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800">
             Check Storyline
           </button>
+          </Link>
         </div>
       </div>
     </main>
