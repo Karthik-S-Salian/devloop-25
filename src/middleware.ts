005c7c85f5bb7ae88988ab5d99de415f0de13064
Karthik-S-Salian <karthikssalian5@gmail.com>
2025-04-18 06:43:25 +0530

    fix:middleware check

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import { env } from 'process'
 export function middleware(request: NextRequest) {
     const allowedPath = '/coming-soon'
 
-    // if (!request.nextUrl.pathname.startsWith(allowedPath)) {
+    // if (env.NODE_ENV !== "development" && !request.nextUrl.pathname.startsWith(allowedPath)) {
     //     return NextResponse.redirect(new URL(allowedPath, request.url))
     // }
 
